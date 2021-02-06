051644ced2fc3a581985f138acc8e5f07d3fd38b
Leozin89Python <leozin069@gmail.com>
2021-02-06 19:36:32 -0300

    prototipo-alterado-v1.1.14

--- src/teste-banco-firebase/conexao/src/components/crud/Crud.jsx
+++ src/teste-banco-firebase/conexao/src/components/crud/Crud.jsx
@@ -28,7 +28,9 @@ export default class Crud extends Component {
             tokenInputAltered2: '',
             nameInputAltered2:'',
             ageInputAltered2:'',
-
+            tokenInputDelete:'',
+            nameInputDelete:'',
+            ageInputDelete:''
           }
 
         
@@ -44,8 +46,13 @@ export default class Crud extends Component {
 
           this.altera = this.altera.bind(this);
           this.alteraCondicionado = this.alteraCondicionado.bind(this);
-          this.alteraCondicionado2 = this.alteraCondicionado2.bind(this)
-          this.inseriNovo = this.inseriNovo.bind(this)
+          this.alteraCondicionado2 = this.alteraCondicionado2.bind(this);
+          //this.inseriNovo = this.inseriNovo.bind(this);
+          this.limpaAlteraCondicionado2 =  this.limpaAlteraCondicionado2.bind(this);
+          this. deleteElement = this.deleteElement.bind(this);
+
+
+
           //initialize firebase  
             if(!firebase.apps.length){
               firebase.initializeApp(firebaseConfig);
@@ -90,7 +97,7 @@ export default class Crud extends Component {
     altera = (e) =>{
         firebase.database().ref('token').set(this.state.tokenInput);
         firebase.database().ref('name').set(this.state.nameInput);
-        firebase.database().ref('age').set(this.state.ageInput)
+        firebase.database().ref('age').set(this.state.ageInput);
         
         e.preventDefault();
     }
@@ -102,30 +109,54 @@ export default class Crud extends Component {
 
       e.preventDefault();
     }
-   
+
+   limpaAlteraCondicionado = () =>{
+
+   }
+
     alteraCondicionado2 = (e) =>{
-      
-      
-      this.inseriNovo()
-      e.preventDefault();
-    }
-
-    inseriNovo = (e) =>{
-      firebase.database().ref('clientes').child(1).set(this.state.novoInput);
+      firebase.database().ref('clientes').child(1).child('token').set(this.state.tokenInputAltered2);
+      firebase.database().ref('clientes').child(1).child('name').set(this.state.nameInputAltered2);
+      firebase.database().ref('clientes').child(1).child('age').set(this.state.ageInputAltered2);
+      firebase.database().ref('clientes').child(1).child('sex').set(this.state.novoInput);
 
       e.preventDefault();
     }
 
+    limpaAlteraCondicionado2 = (e) => {
+      this.setState({
+        novoInput:'',
+        tokenInputAltered2: '',
+        nameInputAltered2:'',
+        ageInputAltered2:'',
+      })
+      e.preventDefault();
+    }
 
+    deleteElement = (e) =>   {
 
-
+      e.preventDefault();
+    }
 
 
 
     render(){
 
       
-        const { token, name , age,  tokenInput, nameInput, ageInput, tokenInputAltered, nameInputAltered, ageInputAltered , tokenInputAltered2, nameInputAltered2, ageInputAltered2 } = this.state;
+        const { token, name , age,  
+          tokenInput,
+          nameInput,
+          ageInput,
+          tokenInputAltered,
+          nameInputAltered,
+          ageInputAltered ,
+          tokenInputAltered2,
+          nameInputAltered2,
+          ageInputAltered2,
+          tokenInputDelete,
+          nameInputDelete,
+          ageInputDelete,
+         } = this.state;
         
         
 
@@ -138,18 +169,21 @@ export default class Crud extends Component {
 
             <form onSubmit={this.altera}>
                 <input 
+                 placeholder="token"
                   type="text"
                   value={this.state.tokenInput}
                   onChange={(e) => this.setState({tokenInput: e.target.value})}
                  />
                   
                   <input 
+                  placeholder="nome"
                   type="text"
                   value={this.state.nameInput}
                   onChange={(e) => this.setState({nameInput: e.target.value})}
                  />
 
                  <input 
+                  placeholder="idade"
                   type="number"
                   value={this.state.ageInput}
                   onChange={(e) => this.setState({ageInput: e.target.value})}
@@ -170,18 +204,21 @@ export default class Crud extends Component {
 
             <form onSubmit={this.alteraCondicionado}>
                 <input 
+                  placeholder="token"
                   type="text"
                   value={this.state.tokenInputAltered}
                   onChange={(e) => this.setState({tokenInputAltered: e.target.value})}
                  />
                   
                   <input 
+                  placeholder="nome"
                   type="text"
                   value={this.state.nameInputAltered}
                   onChange={(e) => this.setState({nameInputAltered: e.target.value})}
                  />
 
                  <input 
+                  placeholder="idade"
                   type="number"
                   value={this.state.ageInputAltered}
                   onChange={(e) => this.setState({ageInputAltered: e.target.value})}
@@ -201,33 +238,35 @@ export default class Crud extends Component {
 
           <form onSubmit={this.alteraCondicionado2}>
               <input 
+                placeholder="token"
                 type="text"
                 value={this.state.tokenInputAltered2}
                 onChange={(e) => this.setState({tokenInputAltered2: e.target.value})}
               />
                 
                 <input 
+                placeholder="nome"
                 type="text"
                 value={this.state.nameInputAltered2}
                 onChange={(e) => this.setState({nameInputAltered2: e.target.value})}
               />
 
               <input 
+                placeholder="idade"
                 type="number"
                 value={this.state.ageInputAltered2}
                 onChange={(e) => this.setState({ageInputAltered2: e.target.value})}
               />
 
-              <button type="submit">altera condicionado</button>
-
-
               <input 
+                  placeholder="sexo"
                   type="text"
                   value={this.state.novoInput}
                   onChange={(e) => this.setState({novoInput: e.target.value})}
                  />
-
-                 <button type="submit">novo atributo</button>
+              
+              <button type="submit">altera condicionado</button>
+              <button onClick={this.limpaAlteraCondicionado2}>limpar</button>
           </form>
 
           <h3>token:  { tokenInputAltered2 }</h3>
@@ -235,6 +274,46 @@ export default class Crud extends Component {
           <h3>idade:  { ageInputAltered2  }</h3>
 
           
+
+          <h1>delete</h1>
+
+            <form onSubmit={this.alteraCondicionado2}>
+                <input 
+                  placeholder="token"
+                  type="text"
+                  value={this.state.tokenInputAltered2}
+                  onChange={(e) => this.setState({tokenInputAltered2: e.target.value})}
+                />
+                  
+                  <input 
+                  placeholder="nome"
+                  type="text"
+                  value={this.state.nameInputAltered2}
+                  onChange={(e) => this.setState({nameInputAltered2: e.target.value})}
+                />
+
+                <input 
+                  placeholder="idade"
+                  type="number"
+                  value={this.state.ageInputAltered2}
+                  onChange={(e) => this.setState({ageInputAltered2: e.target.value})}
+                />
+
+                <input 
+                    placeholder="sexo"
+                    type="text"
+                    value={this.state.novoInput}
+                    onChange={(e) => this.setState({novoInput: e.target.value})}
+                  />
+                
+                <button type="submit">altera condicionado</button>
+                <button onClick={this.limpaAlteraCondicionado2}>limpar</button>
+            </form>
+
+            <h3>token:  { tokenInputAltered2 }</h3>
+            <h3>nome:   { nameInputAltered2 }</h3>
+            <h3>idade:  { ageInputAltered2  }</h3>
+
           </div>
       );
     }
