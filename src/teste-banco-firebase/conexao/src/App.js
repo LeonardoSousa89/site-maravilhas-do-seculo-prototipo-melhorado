7ff8099a01fb7655e7fe8a94576482e719ff7612
Leozin89Python <leozin069@gmail.com>
2021-02-06 04:10:34 -0300

    prototipo-alterado-v1.1.10

---
+++ src/teste-banco-firebase/conexao/src/App.js
@@ -0,0 +1,89 @@
+import React,{Component} from 'react'
+import firebase from 'firebase'
+
+import './App.css'
+
+
+
+export default class App extends Component {
+
+    constructor(props){
+      super(props)
+
+        this.state = {
+          token:'...carregando',
+          name:'...nome aqui',
+          age:12
+        }
+
+        
+        let firebaseConfig = {
+           apiKey: "myApiKey",
+           authDomain: "myAuthDomain",
+           databaseURL: "https://teste-de-conexao-57a31-default-rtdb.firebaseio.com/",
+           storageBucket: "myStorageBocket",
+           messagingSenderId: "idhere"   
+          };  
+
+
+          //initialize firebase  
+          if(!firebase.apps.length){
+            firebase.initializeApp(firebaseConfig)
+         }     
+
+       
+         /**  olheiro  em tempo real*/
+         /*
+         
+         firebase.database().ref('token').on('value', (snapshot)  =>{
+           let state = this.state
+           state.token = snapshot.val()
+           this.setState(state)
+         })
+         
+         */
+
+           
+         /**  olheiro  de uma única mudança */
+         firebase.database().ref('token').once('value').then((snapshot)=>{
+          let state = this.state
+          state.token = snapshot.val()
+          this.setState(state)
+         })
+
+         firebase.database().ref('usuarios').child(1).on('value',(snapshot)=>{
+           let state = this.state
+           state.name = snapshot.val()
+           this.setState(state)
+         })
+         
+    }
+
+
+
+    render(){
+
+      const { name , age} = this.state  
+
+      return (
+        <div className="App">
+
+          <h1>
+            token:  { this.state.token }
+          </h1>
+
+          <h1>
+            nome: { name }
+          </h1>
+
+          
+          <h1>
+            idade: { age }
+          </h1>
+
+        </div>
+      );
+    }
+}
+
+
