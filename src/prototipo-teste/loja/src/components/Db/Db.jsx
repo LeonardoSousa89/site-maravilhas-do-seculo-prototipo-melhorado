7fe085ab3a886a2a8960dc5e163e9eaf7f7af66d
Leozin89Python <leozin069@gmail.com>
2021-02-06 01:06:41 -0300

    prototipo-alterado-v1.1.9

---
+++ src/prototipo-teste/loja/src/components/Db/Db.jsx
@@ -0,0 +1,33 @@
+import React, {Component} from 'react'
+import firebase from 'firebase'
+
+
+  export default class Banco extends Component{
+
+      /*
+          firebaseConfig = {
+            apiKey: "AIzaSyCyrBCgj4rhDpZkEZILN-Ka1Anfd9A8Zq0",
+            authDomain: "maravilhas-do-seculo.firebaseapp.com",
+            projectId: "maravilhas-do-seculo",
+            storageBucket: "maravilhas-do-seculo.appspot.com",
+            messagingSenderId: "313534077862",
+            appId: "1:313534077862:web:3b22c375471640f0704d67",
+            measurementId: "G-GNRVBL3SQ7"
+          };
+
+          firebase.initializeApp(firebaseConfig);
+          firebase.analytics();
+
+      */
+        
+
+        render(){
+          return(
+            <React.Fragment>
+
+             
+            </React.Fragment>
+          )
+        }
+
+  }
