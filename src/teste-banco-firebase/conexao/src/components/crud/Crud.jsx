38ab0dbf8b0fe06e4f84e3c98819dda7881d42a4
Leozin89Python <leozin069@gmail.com>
2021-02-06 21:21:45 -0300

    prototipo-alterado-v1.1.15

--- src/teste-banco-firebase/conexao/src/components/crud/Crud.jsx
+++ src/teste-banco-firebase/conexao/src/components/crud/Crud.jsx
@@ -1,6 +1,8 @@
 import React,{Component} from 'react'
 import firebase from 'firebase'
 
+import './Crud.css'
+
 
 
 
@@ -28,9 +30,14 @@ export default class Crud extends Component {
             tokenInputAltered2: '',
             nameInputAltered2:'',
             ageInputAltered2:'',
+            novoInputDelete:'',
             tokenInputDelete:'',
             nameInputDelete:'',
-            ageInputDelete:''
+            ageInputDelete:'',
+            tokenInputCad:'',
+            nameInputCad:'',
+            ageInputCad:'',
+            sexInputCad:''
           }
 
         
@@ -49,14 +56,23 @@ export default class Crud extends Component {
           this.alteraCondicionado2 = this.alteraCondicionado2.bind(this);
           //this.inseriNovo = this.inseriNovo.bind(this);
           this.limpaAlteraCondicionado2 =  this.limpaAlteraCondicionado2.bind(this);
-          this. deleteElement = this.deleteElement.bind(this);
-
+          this. deleteElementToken = this.deleteElementToken.bind(this);
+          this. limpaDeleteElement = this. limpaDeleteElement.bind(this);
+          this.cadastro = this.cadastro.bind(this); 
+          this.limpaCadastro = this.limpaCadastro.bind(this);
 
 
           //initialize firebase  
             if(!firebase.apps.length){
               firebase.initializeApp(firebaseConfig);
           }     
+
+
+
+
+
+
+
 
        
          /**  olheiro  em tempo real*/
@@ -94,6 +110,8 @@ export default class Crud extends Component {
     }
  
 
+
+
     altera = (e) =>{
         firebase.database().ref('token').set(this.state.tokenInput);
         firebase.database().ref('name').set(this.state.nameInput);
@@ -101,6 +119,10 @@ export default class Crud extends Component {
         
         e.preventDefault();
     }
+
+
+
+
 
     alteraCondicionado = (e) =>{
       firebase.database().ref('usuarios').child(1).child('token').set(this.state.tokenInputAltered);
@@ -110,9 +132,18 @@ export default class Crud extends Component {
       e.preventDefault();
     }
 
+
+
+
+
    limpaAlteraCondicionado = () =>{
 
    }
+
+
+
+
+
 
     alteraCondicionado2 = (e) =>{
       firebase.database().ref('clientes').child(1).child('token').set(this.state.tokenInputAltered2);
@@ -122,6 +153,10 @@ export default class Crud extends Component {
 
       e.preventDefault();
     }
+
+
+
+
 
     limpaAlteraCondicionado2 = (e) => {
       this.setState({
@@ -133,7 +168,73 @@ export default class Crud extends Component {
       e.preventDefault();
     }
 
-    deleteElement = (e) =>   {
+
+
+
+
+    deleteElementToken = (e) =>   {
+      firebase.database().ref('clientes2').child(4).child('token').remove();
+
+      e.preventDefault();
+    }
+    deleteElementName = (e) =>{
+        firebase.database().ref('clientes2').child(4).child('name').remove();
+
+        e.preventDefault();
+    }
+    deleteElementAge = (e) => {
+      firebase.database().ref('clientes2').child('4').child('age').remove();
+
+      e.preventDefault();
+    }
+    deleteElementNovoInput = (e) =>{
+      firebase.database().ref('clientes2').child('4').child('sex').remove();
+
+      e.preventDefault();
+    }
+
+
+
+
+
+
+
+    limpaDeleteElement = (e) =>{
+      this.setState({
+        novoInputDelete:'',
+        tokenInputDelete: '',
+        nameInputDelete:'',
+        ageInputDelete:'',
+      })
+
+      e.preventDefault();
+    } 
+
+
+
+
+
+    cadastro = (e) => {
+      let cadastro = firebase.database().ref('cadastro');
+      let chave = cadastro.push().key;
+
+      cadastro.child(chave).set({
+        token:this.state.tokenInputCad,
+        name:this.state.nameInputCad,
+        age:this.state.ageInputCad,
+        sex:this.state.ageInputCad
+      });
+
+      e.preventDefault();
+    }
+
+    limpaCadastro = (e) =>{
+      this.setState({
+        tokenInputCad:'',
+        nameInputCad:'',
+        ageInputCad:'',
+        sexInputCad:''
+      })
 
       e.preventDefault();
     }
@@ -156,6 +257,10 @@ export default class Crud extends Component {
           tokenInputDelete,
           nameInputDelete,
           ageInputDelete,
+          tokenInputCad,
+          nameInputCad,
+          ageInputCad,
+          sexInputCad
          } = this.state;
         
         
@@ -163,9 +268,13 @@ export default class Crud extends Component {
 
 
         return (
+
+
+
           <div className="crud">
 
-              <h1>altera</h1>
+              <h1>Exercicio de CRUD em REACT.JS&copy; + FIREBASE&trade;</h1>
+              <h1> inseri e altera </h1>
 
             <form onSubmit={this.altera}>
                 <input 
@@ -195,12 +304,20 @@ export default class Crud extends Component {
             <h3>token:  { tokenInput }</h3>
             <h3>nome:   { nameInput}</h3>
             <h3>idade:  { ageInput }</h3>
+            <hr/>
+
+
+
+
+
+
+
 
 
 
 
               
-            <h1>altera condicionado + inseri novo atributo </h1>
+            <h1>inseri e altera condicionado + inseri novo atributo </h1>
 
             <form onSubmit={this.alteraCondicionado}>
                 <input 
@@ -231,10 +348,19 @@ export default class Crud extends Component {
             <h3>nome:   { nameInputAltered }</h3>
             <h3>idade:  { ageInputAltered  }</h3>
           
+            <hr/>
           
+
+
+
+
+
+
+
+
+
           
-          
-            <h1>altera condicionado2</h1>
+            <h1>inseri e altera condicionado2</h1>
 
           <form onSubmit={this.alteraCondicionado2}>
               <input 
@@ -266,57 +392,119 @@ export default class Crud extends Component {
                  />
               
               <button type="submit">altera condicionado</button>
-              <button onClick={this.limpaAlteraCondicionado2}>limpar</button>
           </form>
+
+          <button onClick={this.limpaAlteraCondicionado2}>limpar</button>
 
           <h3>token:  { tokenInputAltered2 }</h3>
           <h3>nome:   { nameInputAltered2 }</h3>
           <h3>idade:  { ageInputAltered2  }</h3>
 
-          
+          <hr/>
+
+
+
+
+
+
+
+
 
           <h1>delete</h1>
 
-            <form onSubmit={this.alteraCondicionado2}>
+            <form
+            className="flex">
                 <input 
-                  placeholder="token"
+                  placeholder="será deletado o elemento token 'se houver'"
                   type="text"
-                  value={this.state.tokenInputAltered2}
-                  onChange={(e) => this.setState({tokenInputAltered2: e.target.value})}
+                  value={this.state.tokenInputDelete}
+                  onChange={(e) => this.setState({tokenInputDelete: e.target.value})}
                 />
+                <button onClick={this.deleteElementToken}>deletar</button>
                   
                   <input 
-                  placeholder="nome"
+                  placeholder="será deletado o elemento name"
                   type="text"
-                  value={this.state.nameInputAltered2}
-                  onChange={(e) => this.setState({nameInputAltered2: e.target.value})}
+                  value={this.state.nameInputDelete}
+                  onChange={(e) => this.setState({nameInputDelete: e.target.value})}
                 />
+                 <button onClick={this.deleteElementName}>deletar</button>
 
                 <input 
-                  placeholder="idade"
+                  placeholder="será deletado o elemento age"
                   type="number"
-                  value={this.state.ageInputAltered2}
-                  onChange={(e) => this.setState({ageInputAltered2: e.target.value})}
+                  value={this.state.ageInputDelete}
+                  onChange={(e) => this.setState({ageInputDelete: e.target.value})}
                 />
+                 <button onClick={this.deleteElementAge}>deletar</button>
 
                 <input 
-                    placeholder="sexo"
+                    placeholder="será deletado o elemento sex 'se houver'"
                     type="text"
-                    value={this.state.novoInput}
-                    onChange={(e) => this.setState({novoInput: e.target.value})}
+                    value={this.state.novoInputDelete}
+                    onChange={(e) => this.setState({novoInputDelete: e.target.value})}
                   />
+                   <button onClick={this.deleteElementNovoInput}>deletar</button>
                 
-                <button type="submit">altera condicionado</button>
-                <button onClick={this.limpaAlteraCondicionado2}>limpar</button>
-            </form>
+                   <button onClick={this.limpaDeleteElement}>limpar</button>
+              </form>
 
-            <h3>token:  { tokenInputAltered2 }</h3>
-            <h3>nome:   { nameInputAltered2 }</h3>
-            <h3>idade:  { ageInputAltered2  }</h3>
+              <h3>token:  { tokenInputDelete }</h3>
+              <h3>nome:   { nameInputDelete }</h3>
+              <h3>idade:  { ageInputDelete  }</h3>
+              <hr/>
+
+
+
+
+
+              <h1>Cadastro</h1>
+
+              <form onSubmit={this.cadastro}
+              className="flex">
+                  <input 
+                    placeholder="token"
+                    type="text"
+                    value={this.state.tokenInputCad}
+                    onChange={(e) => this.setState({tokenInputCad: e.target.value})}
+                  />
+
+                    <input 
+                    placeholder="nome"
+                    type="text"
+                    value={this.state.nameInputCad}
+                    onChange={(e) => this.setState({nameInputCad: e.target.value})}
+                  />
+
+                  <input 
+                    placeholder="idade"
+                    type="number"
+                    value={this.state.ageInputCad}
+                    onChange={(e) => this.setState({ageInputCad: e.target.value})}
+                  />
+
+                  <input 
+                      placeholder="sex"
+                      type="text"
+                      value={this.state.sexInputCad}
+                      onChange={(e) => this.setState({sexInputCad: e.target.value})}
+                    />
+                    <button type="submit">cadastrar</button>
+                </form>
+
+                <button className="flex" onClick={this.limpaCadastro}>limpar</button>
+               
+
+                <h3>token:  { tokenInputCad }</h3>
+                <h3>nome:   { nameInputCad }</h3>
+                <h3>idade:  { ageInputCad  }</h3>
+                <h3>sex:    { sexInputCad  }</h3>
+
+
 
           </div>
-      );
-    }
+       );
+   }
 }
 
 
